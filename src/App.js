3170881a296e11f50b8f374352b346762f382b76
Jake Butler <tjakebutler@gmail.com>
2021-02-04 20:15:27 -0600

    install axios, using the opentdb api for requesting questions and answers, currently the app is rendering flashcards to the screen and the questions are displaying as well as the answers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FlashcardList from './FlashcardList';
 import './app.css'
+import axios from 'axios'
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  useEffect(() => {
+    axios.get('https://opentdb.com/api.php?amount=10')
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = questionItem.correct_answer
+        const options = [...questionItem.incorrect_answers, answer]
+
+            // individual flash card element
+          return  {
+            id: `${index}-${Date.now()}`,
+            question: questionItem.question,
+            answer: answer,
+            //          50% of the time we have a negative number and 50% positive number
+            options: options.sort(() => Math.random() - .5)
+          }
+      }))
+      console.log('this is res!', res)
+      console.log('this is res.data', res.data)
+    })
+  }, [])
+
+
+
+
+
+
   return (
     <FlashcardList flashcards={flashcards}/>
   );
